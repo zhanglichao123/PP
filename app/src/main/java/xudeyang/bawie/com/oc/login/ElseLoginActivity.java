802ca94062aa812a55712b3953de08546af755de
zhanglichao123 <1051085185@qq.com>
2018-06-10 18:26:21 +0800

    zhnga

---
+++ app/src/main/java/xudeyang/bawie/com/oc/login/ElseLoginActivity.java
@@ -0,0 +1,15 @@
+package xudeyang.bawie.com.oc.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xudeyang.bawie.com.oc.R;
+
+public class ElseLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_else_login);
+    }
+}
--- app/src/main/java/xudeyang/bawie/com/oc/view/activity/LoginActivity.java
+++ app/src/main/java/xudeyang/bawie/com/oc/view/activity/LoginActivity.java
@@ -9,11 +9,14 @@ import xudeyang.bawie.com.oc.R;
 import xudeyang.bawie.com.oc.utils.TitleBar;
 import xudeyang.bawie.com.oc.view.base.BaseActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener{
 
 
     TitleBar titlebarLoginlogin;
     private TextView text;
+    private TextView qq;
+    private TextView weixin;
+    private TextView elsel;
 
     @Override
     public void initView() {
@@ -30,8 +33,16 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void initHttp() {
+
          titlebarLoginlogin = findViewById(R.id.titlebar_loginlogin);
+         qq = findViewById(R.id.qq_login);
+         weixin = findViewById(R.id.weixin_long);
+         elsel = findViewById(R.id.else_login);
+         qq.setOnClickListener( this);
+         titlebarLoginlogin = findViewById(R.id.titlebar_loginlogin);
+         //返回
          titlebarLoginlogin.setBack(this);
+         //设置右边的字
          text = titlebarLoginlogin.getText();
          text.setVisibility(View.GONE);
 
@@ -43,4 +54,16 @@ public class LoginActivity extends BaseActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+                case R.id.qq_login:
+                break;
+                case R.id.weixin_long:
+                break;
+                case R.id.else_login:
+
+                break;
+        }
+    }
 }
